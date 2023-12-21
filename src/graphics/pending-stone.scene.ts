0568e118f60ed63083d368960f9e9e896efb93fc
Eugene Astroner <zhenyanikforov@gmail.com>
2023-12-21 17:17:40 +0200

    pending stone change

--- src/graphics/pending-stone.scene.ts
+++ src/graphics/pending-stone.scene.ts
@@ -3,20 +3,15 @@ import { PreloadableScene } from "./preloadable-scene.class";
 import { StoneScene } from "./stone.scene";
 
 export class PendingStoneScene extends PreloadableScene({ dependencies: [StoneScene] }) {
-    private highStone = new StoneScene(undefined, undefined, true);
-    private lowStone = new StoneScene(undefined, true);
+    private stone = new StoneScene(undefined, true);
 
     constructor() {
         super();
-
-        this.highStone.position.set(0, .4, 0);
-        this.add(this.highStone);
-
-        this.add(this.lowStone);
+        
+        this.add(this.stone);
     }
 
     setColor(color: "black" | "white") {
-        this.highStone.setColor(color);
-        this.lowStone.setColor(color);
+        this.stone.setColor(color);
     }
 }
